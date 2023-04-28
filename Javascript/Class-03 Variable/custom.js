f115aca41dfa3c0fb9ec9d66537ee0e33da2b3e0
Fahim Hossain <fahimhossain655@gmail.com>
2023-04-28 21:20:47 +0600

    all codes

---
+++ Javascript/Class-03 Variable/custom.js
@@ -0,0 +1,30 @@
+"use strict"
+
+/*alert("Hello World");
+
+document.write("How Are You");
+
+let myNameis = "Fahim Hossain Ovil";
+
+alert(myNameis);
+
+let $1234 = "Is That You";
+
+console.log($1234);
+
+let x =10, y =20, z = (x+y);
+
+alert(z);
+
+let yourNameis = "Karim";
+
+console.log(yourNameis);*/
+
+/*String Type Data*/
+
+let strtype = `Hello fahim`;
+
+console.log(strtype);
+
+alert(typeof strtype);
+    
