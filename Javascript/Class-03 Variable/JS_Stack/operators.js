543b8784d4c9e91709f8918a92c8e6dc96db1e05
Fahim Hossain <fahimhossain655@gmail.com>
2023-06-16 21:52:50 +0600

    Update operators.js

--- Javascript/Class-03 Variable/JS_Stack/operators.js
+++ Javascript/Class-03 Variable/JS_Stack/operators.js
@@ -1,11 +1,34 @@
 // Arithmetic operators
-console.log("arithmetic Operators");
+// console.log("arithmetic Operators");
 
-let a = 20
-let b = 10
+// let a = 20
+// let b = 10
 
-console.log(a + "+" + b + "=" + (a+b));    //20+10=30
-console.log(a  + "-" + b + "=" + (a-b));   //20-10=10
-console.log(a + "*" + b + "=" + (a*b));    //0*10=200
-console.log(a + "/" + b + "=" + (a/b));    //20/10=2
+// console.log(a + "+" + b + "=" + (a+b));    //20+10=30
+// console.log(a  + "-" + b + "=" + (a-b));   //20-10=10
+// console.log(a + "*" + b + "=" + (a*b));    //0*10=200
+// console.log(a + "/" + b + "=" + (a/b));    //20/10=2
+// console.log(a + "%" + b + "=" + (a%b));    //20%10=0
+
+// Assignment operators
+
+// console.log("Assignment operators");
+
+// let asn  = 10;
+
+// console.log("a ="  + asn);
+// asn += 5;
+// console.log("a +=" + asn);
+// asn -= 5;
+// console.log("a -=" + asn);
+// asn *= 5;
+// console.log("a *=" + asn);
+// asn /= 5;
+// console.log("a /=" + asn);
+// asn %= 5;
+// console.log("a %=" +  asn);
+
+// Rellational Operators
+
+console.log("Relational Operators")
 
